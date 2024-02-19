79a71a9adaaae1b2216020200f3369f39cfefb99
Felix G. <82956884+felix12123@users.noreply.github.com>
2024-02-19 16:07:28 +0000

    Add devcontainer configuration and create media directories

--- main.jl
+++ main.jl
@@ -1,5 +1,5 @@
 # neccecary imports
-using Plots, Test, Statistics, Random, QuadGK, ThreadsX, SpecialFunctions, DataFrames, LsqFit, RollingFunctions
+using Plots, Test, Statistics, Random, QuadGK, ThreadsX, SpecialFunctions, DataFrames, LsqFit, RollingFunctions, Distributions
 # later removable imports
 using HTTP, BenchmarkTools
 
@@ -13,6 +13,14 @@ include("test/runtests.jl")
 include("tasks/A1.jl")
 include("tasks/A2.jl")
 include("tasks/A3.jl")
+include("tasks/A4.jl")
+
+directories = ["media", "media/A1", "media/A2", "media/A3", "media/A4"]
+for dir in directories
+	if !isdir(dir)
+		mkdir(dir)
+	end
+end
 
 
 println("Threads: ", Threads.nthreads())
